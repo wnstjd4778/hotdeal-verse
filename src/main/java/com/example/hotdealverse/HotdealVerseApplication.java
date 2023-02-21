d5e842339121bb4f508ff8757f48ca7c0d909646
wnstjd4778 <smy4778@naver.com>
2023-02-21 12:20:24 +0900

    first commit

---
+++ src/main/java/com/example/hotdealverse/HotdealVerseApplication.java
@@ -0,0 +1,13 @@
+package com.example.hotdealverse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HotdealVerseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HotdealVerseApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/hotdealverse/HotdealVerseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.hotdealverse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HotdealVerseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
